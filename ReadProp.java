95adb969f049fb9ed564e9ddd39c7711a9766770
azEsm <v.kirill.01@gmail.com>
2014-03-25 12:47:59 +0400

    refactored

--- ReadProp.java
+++ ReadProp.java
@@ -12,7 +12,6 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -21,22 +20,43 @@ import java.util.regex.Pattern;
  */
 public class ReadProp {
     
-    private static final Pattern pattern = Pattern.compile("^([^#=]+)=([^\\n\\r]+)");
+    enum Symbol
+ 
+    {
+        COMMENT("^#.*$"),
+        PNAME("[^:=]+"),
+        DELIMITER("[:=]"),
+        PVALUE(".*$");
+
+        final Pattern pattern;
+        Symbol(String exp)
+        {
+            this.pattern = Pattern.compile(exp);
+        }
+    }
     
     public static Map<String, String> readProp(String file) throws FileNotFoundException {
-        Map<String, String> result = new HashMap<String, String>();
+        Map<String, String> result = new HashMap<>();
         String pname;
         String pvalue;
         File prop = new File(file);
         Scanner line = new Scanner(prop);
 
         while(line.hasNext()) {
-            Matcher matcher = pattern.matcher(line.nextLine());
-            while(matcher.find()) {
-                pname = matcher.group(1).trim();
-                pvalue = matcher.group(2).trim();
+            if (line.hasNext(Symbol.COMMENT.pattern)) {
+                // Ignoring the comment
+                line.next(Symbol.COMMENT.pattern);
+            }
+            else if (line.hasNext(Symbol.PNAME.pattern))
+            {
+                pname = line.next(Symbol.PNAME.pattern);
+                // Pull the delimiter off the stream and ignore it.
+                line.next(Symbol.DELIMITER.pattern);
+                //No reading the property value
+                pvalue = line.next(Symbol.PVALUE.pattern);
                 result.put(pname, pvalue);
-            }            
+                line.nextLine();
+            }        
         }
         return result;
     }
